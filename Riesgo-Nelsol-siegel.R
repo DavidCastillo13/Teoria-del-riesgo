2b01e764e13b06041b7412440dd00cef6cbe49ac
David Castillo <111095536+DavidCastillo13@users.noreply.github.com>
2022-10-12 13:39:01 -0500

    Add files via upload

--- Riesgo-Nelsol-siegel.R
+++ Riesgo-Nelsol-siegel.R
@@ -14,7 +14,7 @@ tau= 3
 x1=c()
 x2=c()
 for (i in 1:9) {
-  meses= c(1,3,6,9,12,24,12*5,120,240)
+  meses= c(1,3,6,9,12,24,60,120,360)
   años=meses/12
   x1[i] = (1-exp(-años[i]/tau))/(años[i]/tau)
   x2[i] = ((1-exp(-años[i]/tau))/(años[i]/tau))-exp(-años[i]/tau)
@@ -29,8 +29,8 @@ for (i in 1:9) {
   ns[i]=m1$coefficients[1] + m1$coefficients[2]*x1[i] + m1$coefficients[3]*x2[i]
 }
 
-plot(ns, type = "l", main = "Curva estructura a termino", ylab = "tasa spot", xlab = "años" )
-lines(tasa,type="l",col="red",lty=2)
+plot(años,ns, type = "l", main = "Curva estructura a termino", ylab = "tasa spot", xlab = "años" , ylim=c(0,3), xlim = c(años[1],años[9]))
+lines(años,tasa,type="l",col="red",lty=2)
 legend("topleft", legend = c("Nelson-Siegel","Real"),
        col=c("black", "red"),
        lty = c("solid", "solid"), bty = "n")
@@ -41,8 +41,10 @@ t.test(m1$residuals)
 shapiro.test(m1$residuals)
 bptest(m1) 
 
-rmse(tasa, ns)
+plot(tasa,años, type = "l")
 
+rmse(tasa, ns)
+12*5
 #### Gracias por la lectura
 
   
